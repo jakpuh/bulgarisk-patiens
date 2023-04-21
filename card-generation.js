cc30b57f66f876bd28fcd44efb707e4e0dde95b8
Christopher <christopher.balter@skola.taby.se>
2023-04-21 13:12:29 +0200

    add very much very good

---
+++ card-generation.js
@@ -0,0 +1,13 @@
+/*const stacks = [2,3,4,5,6];
+
+
+function 
+for (i of stacks){
+
+}
+document.getElementById
+*/
+
+var root = document.querySelector(":root");
+let containerWidth = `${100/document.querySelectorAll(".flex-column-container").length}%`;
+root.style.setProperty("--item-width", containerWidth); 
