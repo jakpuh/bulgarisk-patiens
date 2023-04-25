b93991570928ee696324ad11479ec34f61a2895c
jakpuh <jakob.puhl@skola.taby.se>
2023-04-25 11:52:27 +0200

    refactoring

--- script.js
+++ script.js
@@ -13,35 +13,51 @@ function pushSizeSorted(kort) {
     return kort.slice(0, index).concat(kort.length).concat(kort.slice(index));
 }
 
-function drag(kort) {
-    return removeEmpty(pushSizeSorted(reduce(kort)));
-}
-
 function removeEmpty(kort) {
     return kort.filter(val => val != 0);
 }
 
-korten = [3,2,2,54,5,43,42,5,2,6,6,2,8,4321,4,636,4839,2,32,43,424,3,231,372,5434,463,3232,56543]
-// korten = [4, 3];
-dp = {}
-
-let start = Date.now();
-for (var i = 0;; ++i) {
-    // console.log(korten)
-    korten = drag(korten);
-    const kortenStr = JSON.stringify(korten);
-    if (kortenStr in dp) {
-        if (dp[kortenStr] === i - 1) {
-            console.log("patiensen har gått ut :(");
-        } else {
-            console.log(`patiensen går inte ut :) med än cykel på ${i - dp[kortenStr]} drag`);
-        }
-        break;
-    }
-    dp[kortenStr] = i;
+function drag(kort) {
+    return removeEmpty(pushSizeSorted(reduce(kort)));
 }
-let end = Date.now();
-let time = end - start;
 
-console.log(`${i} iterations took ${time} ms`);
-console.log(`Average time per iteration: ${time / i * 1000} ns`);
+function run(korten) {
+    const stacksSeen = {}
+    for (let i = 0;; ++i) {
+        korten = drag(korten);
+        const kortenStr = JSON.stringify(korten);
+        if (kortenStr in stacksSeen) {
+            if (stacksSeen[kortenStr] === i - 1) {
+                return { 
+                    won: false, 
+                    iterations: i, 
+                    cykel: i - stacksSeen[kortenStr] 
+                };
+            } else {
+                return { 
+                    won: true, 
+                    iterations: i, 
+                    cykel: i - stacksSeen[kortenStr] 
+                };
+            }
+        }
+        stacksSeen[kortenStr] = i;
+    }
+}
+
+function takeTime(callback) {
+    const start = Date.now();
+    const ret = callback();
+    const end = Date.now();
+    return [ ret, end - start ];
+}
+
+const korten = [3,2,2,54,5,43,42,5,2,6,6,2,8,4321,4,636,4839,2,32,43,424,3,231,372,5434,463,3232,56543]
+const [res, time] = takeTime(() => { return run(korten) });
+
+if (res.won)
+    console.log("Patiensen går inte ut :) med en cykel på ${res.cykel}")
+else
+    console.log("Patiensen har gått ut :(");
+console.log(`${res.iterations} iterations took ${time} ms`);
+console.log(`Average time per iteration: ${time / res.iterations * 1000} ns`);
