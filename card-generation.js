58c2f8ef68fb8fcc10f2c04bb4b76d4b8a42e589
jakpuh <94695364+jakpuh@users.noreply.github.com>
2023-05-02 21:11:18 +0200

    Add files via upload

--- card-generation.js
+++ card-generation.js
@@ -3,13 +3,25 @@ import { nextDrag } from "./script.js";
 const root = document.querySelector(":root");
 const containerRow = document.querySelector(".flex-row-container")
 
+class StackRed {
+    constructor(count) {
+        this.count = count;
+    }
+};
+
+class StackBlue {
+    constructor(interval) {
+        this.interval = interval;
+    }
+};
+
 function setWidth(cardStackCount) {
     const container = document.querySelector(".flex-column-container");
     let containerWidth = `${100/cardStackCount}%`;
     root.style.setProperty("--item-width", containerWidth);
 }
 
-function addCard(column) {
+function createRedCard(column) {
     const newCard = document.createElement('img');
     newCard.classList.add('img');
     column.appendChild(newCard);
@@ -17,24 +29,87 @@ function addCard(column) {
     newCard.alt = "kort";
 }
 
+function createBlueCard(column) {
+    const newCard = document.createElement('img');
+    newCard.classList.add('img');
+    column.appendChild(newCard);
+    newCard.src = "baksida-kort-blue.png";
+    newCard.alt = "kort";
+}
+
 function displayCardStacks(cardStacks) {
     containerRow.replaceChildren();
-    for (const stackHeight of cardStacks) {
+    for (const stack of cardStacks) {
         const newColumn = document.createElement('div');
         newColumn.classList.add("flex-column-container");
-        for (let i = 0; i < stackHeight; ++i) {
-            addCard(newColumn);
+        if (stack instanceof StackRed) {
+            for (let i = 0; i < stack.count; ++i) {
+                createRedCard(newColumn);
+            }
+        } else {
+            createBlueCard(newColumn);
         }
         containerRow.appendChild(newColumn);
     }
-    setWidth(cardStacks.length);
+    // TODO: check this out
+    // Doesn't work for some reason 
+    // setWidth(cardStacks.length);
 }
+
+
+// interval should be an array of 2 indices, the start index and the end index [start, end); both indices should be 0 based
+function getStacksToDisplay(cardStacks, interval) {
+    if (interval[0] >= cardStacks.length || interval[0] > interval[1]) return [];
+
+    const len = Math.min(cardStacks.length, interval[1]) - interval[0];
+    const restrictedLen = Math.min(len, 20);
+
+    const avg = Math.floor(len / restrictedLen);
+    const rest = len % restrictedLen;
+
+    const avgArray = Array(restrictedLen).fill(avg);
+    const tmpResult = avgArray.slice(0, rest)
+                           .map(val => val + 1)
+                           .concat(avgArray.slice(rest));
+
+    let result = [];
+    let currentStack = interval[0];
+    for (let i = 0; i < restrictedLen; ++i) {
+        if (tmpResult[i] > 1) {
+            result.push(new StackBlue([currentStack, currentStack + tmpResult[i]]));
+        } else {
+            result.push(new StackRed(cardStacks[i]));
+        }
+        currentStack += tmpResult[i];
+    }
+
+    return result;
+}
+
+// let arr = [];
+// for (let i = 0; i < 121; ++i) {
+//     arr.push(i);
+// }
+// const res = (getStacksToDisplay(arr, [1, 25]));
+// res.forEach((elem) => {
+//     console.log(elem.interval);
+// })
+// console.log(res);
 
 (async() => {
     console.log("Running");
-    // displayCardStacks([4,3]);
-    for (const [kort, iteration, subiteration] of nextDrag([4,3])) {
-        await new Promise(r => setTimeout(r, 1000));
+    let start = [];
+    for (let i = 1; i < 123; ++i) {
+        start.push(4);
+    }
+    // Make nextDrag return a object of a class instead
+    for (const [kort, iteration, subiteration] of nextDrag(start)) {
+        displayCardStacks(getStacksToDisplay(kort, [0, kort.length]));
         console.log(kort, iteration, subiteration);
+        if(subiteration == 0) {
+            await new Promise(r => setTimeout(r, 100));
+        } else {
+            await new Promise(r => setTimeout(r, 100));
+        }
     }
 })();
